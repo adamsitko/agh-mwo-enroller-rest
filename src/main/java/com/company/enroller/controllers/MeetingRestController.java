7d4d6bed20fb2e4c6cc96a3a38edc10525fb6af5
unknown <adamsitko1@op.pl>
2018-05-17 18:33:04 +0200

    second meeting commit

--- src/main/java/com/company/enroller/controllers/MeetingRestController.java
+++ src/main/java/com/company/enroller/controllers/MeetingRestController.java
@@ -29,16 +29,16 @@ public class MeetingRestController {
 		return new ResponseEntity<Collection<Meeting>>(meetings, HttpStatus.OK);
 	}
 	
-	/*@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public ResponseEntity<?> getMeeting(@PathVariable("id") String login) {
-	     Participant participant = participantService.findByLogin(login);
-	     if (participant == null) {
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> getMeeting(@PathVariable("id") String id) {
+	     Meeting meeting = meetingService.findById(id);
+	     if (meeting == null) {
 	         return new ResponseEntity(HttpStatus.NOT_FOUND);
 	     }
-	     return new ResponseEntity<Participant>(participant, HttpStatus.OK);
+	     return new ResponseEntity<Meeting>(meeting, HttpStatus.OK);
 	 }
 	
-	 @RequestMapping(value = "", method = RequestMethod.POST)
+	/*@RequestMapping(value = "", method = RequestMethod.POST)
 	 public ResponseEntity<?> registerParticipant(@RequestBody Participant participant){
 		 if (participantService.findByLogin(participant.getLogin()) != null) {
 			 return new ResponseEntity("Unable to create. A participant with login " + participant.getLogin() + " already exist.", HttpStatus.CONFLICT);
